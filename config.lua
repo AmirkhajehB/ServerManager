263237f85b9725db14d5b9fbc83dc18711c4a7f8
AmirkhajehB <amirkhajeh.b@gmail.com>
2016-11-09 21:54:52 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+bot_token = "239047385:AAEbPXGvm7Hs4mQS2CNx2AVOBLLa2Jo-Q_w"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "1.7"
+sudo_name = "khajeh_amir"
+sudo_id = 216507730
+sudo_user = "khajeh_amir"
+nerkh = "5"
